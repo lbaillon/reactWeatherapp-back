3de31df79ee418b7d729221e95ea4db5570034db
Laury Baillon <laury-baillon@hotmail.fr>
2025-02-19 11:06:24 +0100

    attempt to solve cors issues

--- app.js
+++ app.js
@@ -11,11 +11,24 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 const cors = require('cors');
-app.use(cors({
-  origin: "*",
-  methods: "GET,HEAD,OPTIONS,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type, Authorization"
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Autorise toutes les origines
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  res.setHeader(
+      "Content-Security-Policy",
+      "default-src *; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; img-src *; style-src * 'unsafe-inline';"
+  );
+
+  next();
+});
+
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,7 +38,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.options("*", cors());
 
 module.exports = app;
